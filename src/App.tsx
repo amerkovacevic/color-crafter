8bbbb06b5d3bcb555681d6f32da6296e683ac9ba
amerkovacevic <amerkovacevic99@gmail.com>
2025-11-02 14:12:16 -0600

    universal footer try 3

--- src/App.tsx
+++ src/App.tsx
@@ -606,7 +606,7 @@ function App() {
         </div>
       )}
 
-      <footer className="w-full border-t border-tertiary-500/30 bg-primary-800/80 py-4 text-center text-xs text-quaternary-500">
+      <footer className="relative w-full border-t border-tertiary-500/30 bg-primary-800/80 py-4 text-center text-xs text-quaternary-500">
         <p>&copy; {new Date().getFullYear()} Amer Kovacevic All rights reserved.</p>
       </footer>
     </div>
